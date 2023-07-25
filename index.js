9ec7d867244255f679be8403222b88ad5842830b
Hakjae415 <hakjae.k415@gmail.com>
2023-07-25 15:58:35 -0400

    messed around with css

--- index.js
+++ index.js
@@ -1,64 +1,68 @@
+// Create container variable to grab #root from HTML
 const container = document.getElementById("root");
 
+// Create circles variable to hold an array of objects for circles
 const circles = [
     {
         id:1,
         name: "Circle1",
         img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Circle_-_black_simple.svg/500px-Circle_-_black_simple.svg.png?20070329024902",
-        backgroundColor: "#451252"
+        backgroundColor: "#150252"
 
     },
     {
         id:2,
         name: "Circle2",
         img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Circle_-_black_simple.svg/500px-Circle_-_black_simple.svg.png?20070329024902",
-        backgroundColor: "#451252"
+        backgroundColor: "#901252"
     },
     {
         id:3,
         name: "Circle3",
         img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Circle_-_black_simple.svg/500px-Circle_-_black_simple.svg.png?20070329024902",
-        backgroundColor: "#451252"
+        backgroundColor: "#511652"
     }
 ];
 
+// Create squares variable to hold an array of objects for squares
 const squares = [
     {
         id:4,
         name: "Square1",
         img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/500px-Square_-_black_simple.svg.png?20210311042606",
-        backgroundColor: "#451252"
+        backgroundColor: "#251252"
     },
     {
         id:5,
         name: "Square2",
         img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/500px-Square_-_black_simple.svg.png?20210311042606",
-        backgroundColor: "#451252"
+        backgroundColor: "#451202"
     },
     {
         id:6,
         name: "Square3",
         img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/500px-Square_-_black_simple.svg.png?20210311042606",
-        backgroundColor: "#451252"
+        backgroundColor: "#422252"
     }
 ];
 
+// Create App component
 const App = () => {
+    // Create state "color" & function "setColor", "color" stores current value of the state which is empty, setColor allows color to be updated 
     const [color, setColor] = React.useState();
-
     const changeColor = () => {
         const rngColor = "#" + Math.random().toString(16).slice(2,8)
         setColor(rngColor)
     };
 
+    // Create state "counter" & function "useCounter"
     const [counter, useCounter] = React.useState(0);
-
     const count = () => {
         useCounter(counter+1)
     };
 
+    // Create state "increment" & function "useIncrement"
     const [increment, useIncrement] = React.useState(0);
-
     const increase = () => {
         useIncrement(increment+1)
     };
@@ -67,7 +71,7 @@ const App = () => {
         <section>
             <h1>SHAPES</h1>
             <h2># Of Times Circle Has Been Clicked & Changed Colors: {counter}</h2>
-            <h2># Of Times Square Has Been Clicked & Changed Colors: {increment}</h2>
+
             <ul>
                 {
                     circles.map((singleCircle) => {
@@ -83,6 +87,7 @@ const App = () => {
 
                     )}
             </ul>
+            <h2># Of Times Square Has Been Clicked & Changed Colors: {increment}</h2>
             <ul>        
                 {
                     
@@ -102,6 +107,7 @@ const App = () => {
     )
 };
 
+// Additional components
 const Shapes1 = ({singleCircle, count, changeColor, color}) => {
     return (
             <div className="circle" idname={singleCircle.name}>
@@ -115,6 +121,7 @@ const Shapes1 = ({singleCircle, count, changeColor, color}) => {
     )
 };
 
+// Yay more components
 const Shapes2 = ({singleSquare, increase, changeColor, color}) => {
     return (
             <div className="square" idname={singleSquare.name}>
